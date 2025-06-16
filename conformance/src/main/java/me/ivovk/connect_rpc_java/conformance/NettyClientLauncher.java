ba7cb463755231feb37b4d1c747378b98172557f
Ihor Vovk <adwolf15@gmail.com>
2025-06-16 22:56:32 +0200

    Work on ConnectRPC client

---
+++ conformance/src/main/java/me/ivovk/connect_rpc_java/conformance/NettyClientLauncher.java
@@ -0,0 +1,16 @@
+package me.ivovk.connect_rpc_java.conformance;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NettyClientLauncher {
+
+  private static final Logger logger = LoggerFactory.getLogger(NettyClientLauncher.class);
+
+  public static void main(String[] args) {
+    // The client launcher is not implemented yet.
+    // This is a placeholder for future implementation.
+    logger.info("Netty Client Launcher is not implemented yet.");
+  }
+
+}
