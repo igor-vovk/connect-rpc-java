9cc86694e7f74e393f3e83d5df94b8ac73cd8f0f
Ihor Vovk <adwolf15@gmail.com>
2025-06-30 18:26:07 +0200

    Refactor Dockerfile and Makefile for improved build process and add launcher argument validation

--- conformance/src/main/java/me/ivovk/connect_rpc_java/conformance/Launcher.java
+++ conformance/src/main/java/me/ivovk/connect_rpc_java/conformance/Launcher.java
@@ -3,7 +3,11 @@ package me.ivovk.connect_rpc_java.conformance;
 public class Launcher {
 
   public static void main(String... args) throws Exception {
-    var launcher = System.getenv("LAUNCHER");
+    if (args.length == 0) {
+      System.err.println("Please provide the launcher argument");
+      System.exit(1);
+    }
+    var launcher = args[0];
 
     switch (launcher) {
       case "netty-server" -> NettyServerLauncher.main(args);
